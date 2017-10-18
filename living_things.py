b6303ed8c064db8146d08fd08e10108943a928c1
johnashu <johnashu1@hotmail.com>
2017-10-18 13:22:55 +0200

    .

---
+++ living_things.py
@@ -0,0 +1,43 @@
+
+class LivingThing():
+
+    def __init__(self, name, legs, age, health):
+        self.name = name
+        self.legs = legs
+        self.age = age
+        self.health = health
+        self.hunger = 0
+
+    def DamageHealth(self, dmgAmount):
+        self.health = self.health - dmgAmount
+        if self.health == 0:
+            print(self.name, ' Is DeeD!!!')
+
+
+class Canine(LivingThing):
+
+    def __init__(self, furType, wild):
+        self.furType = furType
+        self.wild = wild
+
+    def FurType(self, fur):
+        fur = self.furType
+        print(fur)
+
+    def isWild(self, wild):
+        if self.wild is True:
+            print("Be CAreful, This is a Wild Animal!!")
+        else:
+            print('Feel Free To Pet My Pet Dog!!')
+
+
+john = LivingThing('john', 2, 36, 65)
+john.DamageHealth(65)
+dog = LivingThing('fido', 4, 5, 86 )
+dog = Canine('Long-Haired', True)
+
+print(dog.FurType)
+print(dog.isWild)
+
+print(john.legs)
+print(john.health)
