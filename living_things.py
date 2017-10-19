6b37c93bb6ac5e87bedb1f3232934df2addde5bc
johnashu <johnashu1@hotmail.com>
2017-10-19 18:34:40 +0200

    .

--- living_things.py
+++ living_things.py
@@ -13,7 +13,7 @@ class LivingThing:
         if self.health == 0:
             print(self.name, ' Is DeeD!!!')
         elif dmgAmount > 0:
-            print("Ouch..", self.name, "Took ", dmgAmount, "of Damage\n\n", self.name, 'New Health is: ', self.health)
+            print("Ouch..", self.name, "Took ", dmgAmount, "of Damage\n\n" + self.name + '\'s New Health is: ', self.health, '\n\n')
 
 
 class Canine(LivingThing):
@@ -27,23 +27,53 @@ class Canine(LivingThing):
         self.furType = furType
         self.wild = wild
 
-    def FurType(self, furType):
+    def FurType(self):
         if self.furType == self.furType:
-            print(furType)
+            print(self.furType)
 
-    def isWild(self, wild):
+    def isWild(self):
         if self.wild is True:
             print("Be CAreful, This is a Wild Animal!!")
         else:
-            print('Feel Free To Pet My Pet Dog!!')
+            print('Feel Free To Pet My Dog!!')
+
+
+class Feline(LivingThing):
+
+    def __init__(self, furType, wild, name, legs, age, health):
+        self.name = name
+        self.legs = 4
+        self.age = age
+        self.health = health
+        self.hunger = 0
+        self.furType = furType
+        self.wild = wild
+
+    def FurType(self):
+        if self.furType == self.furType:
+            print(self.furType)
+
+    def isWild(self):
+        if self.wild is True:
+            print("Be Careful, This is a Wild Animal!!")
+        else:
+            print('Feel Free To Pet My Cat!!')
+
 
 
 john = LivingThing('John', 2, 36, 65)
 john.DamageHealth(15)
-dog = Canine('Long-Haired', True, 'fido', 4, 5, 86)
 
-print(dog.FurType)
-print(dog.isWild)
+
+dog = Canine('Long-Haired', True, 'Fido', 4, 5, 86)
+cat = Feline('Short-Haired', False, 'Gotham', 4, 5, 86)
+
+print('\nThe Dog\'s fur is ', dog.FurType())
+print(dog.isWild())
+
+print('\nThe Cat\'s fur is ', cat.FurType())
+print(cat.isWild())
 
 print(john.legs)
 print(dog.legs)
+print(cat.legs)
