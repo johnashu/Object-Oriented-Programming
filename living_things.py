6fc9b8744d452b35d155fd8c1075047de5ae4c9f
johnashu <johnashu1@hotmail.com>
2017-10-19 18:26:48 +0200

    .

--- living_things.py
+++ living_things.py
@@ -1,5 +1,5 @@
 
-class LivingThing():
+class LivingThing:
 
     def __init__(self, name, legs, age, health):
         self.name = name
@@ -12,17 +12,24 @@ class LivingThing():
         self.health = self.health - dmgAmount
         if self.health == 0:
             print(self.name, ' Is DeeD!!!')
+        elif dmgAmount > 0:
+            print("Ouch..", self.name, "Took ", dmgAmount, "of Damage\n\n", self.name, 'New Health is: ', self.health)
 
 
 class Canine(LivingThing):
 
-    def __init__(self, furType, wild):
+    def __init__(self, furType, wild, name, legs, age, health):
+        self.name = name
+        self.legs = 4
+        self.age = age
+        self.health = health
+        self.hunger = 0
         self.furType = furType
         self.wild = wild
 
-    def FurType(self):
-        self.furType = self.furType
-        print(self.furType)
+    def FurType(self, furType):
+        if self.furType == self.furType:
+            print(furType)
 
     def isWild(self, wild):
         if self.wild is True:
@@ -31,13 +38,12 @@ class Canine(LivingThing):
             print('Feel Free To Pet My Pet Dog!!')
 
 
-john = LivingThing('john', 2, 36, 65)
-john.DamageHealth(65)
-dog = LivingThing('fido', 4, 5, 86 )
-dog = Canine('Long-Haired', True)
+john = LivingThing('John', 2, 36, 65)
+john.DamageHealth(15)
+dog = Canine('Long-Haired', True, 'fido', 4, 5, 86)
 
 print(dog.FurType)
 print(dog.isWild)
 
 print(john.legs)
-print(john.health)
+print(dog.legs)
